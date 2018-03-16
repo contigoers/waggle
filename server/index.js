b9fd00dbf655f2be4fea350a1eaa4f808ce9114b
Shannon <schristinep@gmail.com>
2018-03-16 14:17:34 -0500

    adds back empty server request (tryna lose that error in deployed)

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(`${__dirname}/../react-client/dist`));
 
 
-// app.get('/items', (req, res) => {
+ app.get('/items', (req, res) => {
 //   items.selectAll((err, data) => {
 //     if (err) {
 //       res.sendStatus(500);
@@ -17,7 +17,7 @@ app.use(express.static(`${__dirname}/../react-client/dist`));
 //       res.json(data);
 //     }
 //   });
-// });
+ });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port 3000!');
