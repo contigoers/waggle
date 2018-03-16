8f15abcb7c2798923b98f2b135a30a7af9f2d1d3
JP Angelle <jpangelle15@gmail.com>
2018-03-16 15:44:59 -0500

    stands koa server up

--- server/express-index.js
+++ server/express-index.js
@@ -3,22 +3,21 @@ const bodyParser = require('body-parser'); // eslint-disable-line
 
 const items = require('../database-mysql');
 
-
 const app = express();
 
 app.use(express.static(`${__dirname}/../react-client/dist`));
 
 
-//app.get('/items', (req, res) => {
-//   items.selectAll((err, data) => {
-//     if (err) {
-//       res.sendStatus(500);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-//});
+//  app.get('/items', (req, res) => {
+//    items.selectAll((err, data) => {
+//       if (err) {
+//         res.sendStatus(500);
+//      } else {
+//        res.json(data);
+//      }
+//    });
+//  });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on port 3000!');
+  console.log(`listening on port ${3000}!`);
 });
