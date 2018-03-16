b2b60b4190d2e5a6f952aaae90467b697e3ed32b
Shannon <schristinep@gmail.com>
2018-03-16 10:40:11 -0500

    updates schema (shoutout 2 shazam)

--- database/schema.sql
+++ database/schema.sql
@@ -6,19 +6,20 @@
 -- SET FOREIGN_KEY_CHECKS=0;
 
 -- ---
--- Table 'users'
+-- Table 'adopters'
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `users`;
-		
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `adopters`;
+    
+CREATE TABLE `adopters` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
   `name` VARCHAR(50) NULL DEFAULT NULL,
   `pets` BINARY NULL DEFAULT NULL,
   `zipcode` INTEGER(5) NULL DEFAULT NULL,
   `house` ENUM NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`, `user_id`)
 );
 
 -- ---
@@ -27,7 +28,7 @@ CREATE TABLE `users` (
 -- ---
 
 DROP TABLE IF EXISTS `dogs`;
-		
+    
 CREATE TABLE `dogs` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `name` INTEGER(50) NULL DEFAULT NULL,
@@ -42,7 +43,7 @@ CREATE TABLE `dogs` (
   `photo` VARCHAR(150) NULL DEFAULT NULL,
   `org_id` INTEGER NULL DEFAULT NULL,
   `description` VARCHAR(500) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`, `org_id`, `breed`)
 );
 
 -- ---
@@ -51,12 +52,10 @@ CREATE TABLE `dogs` (
 -- ---
 
 DROP TABLE IF EXISTS `orgs`;
-		
+    
 CREATE TABLE `orgs` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `name` VARCHAR(50) NULL DEFAULT NULL,
-  `location` INTEGER(5) NULL DEFAULT NULL,
-  `contact` VARCHAR(50) NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -66,7 +65,7 @@ CREATE TABLE `orgs` (
 -- ---
 
 DROP TABLE IF EXISTS `breed`;
-		
+    
 CREATE TABLE `breed` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `name` VARCHAR(50) NULL DEFAULT NULL,
@@ -80,66 +79,70 @@ KEY ()
 -- ---
 
 DROP TABLE IF EXISTS `favoritedogs`;
-		
+    
 CREATE TABLE `favoritedogs` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `user_id` INTEGER NULL DEFAULT NULL,
   `dog_id` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`, `user_id`, `dog_id`)
 );
 
 -- ---
--- Table 'contacts(maybe)'
+-- Table 'users'
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `contacts(maybe)`;
-		
-CREATE TABLE `contacts(maybe)` (
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(30) NULL DEFAULT NULL,
+  `username` VARCHAR(20) NULL DEFAULT NULL,
+  `password` VARCHAR(20) NULL DEFAULT NULL,
+  `email` VARCHAR(50) NULL DEFAULT NULL,
   `org_id` INTEGER NULL DEFAULT NULL,
   `address` VARCHAR(50) NULL DEFAULT NULL,
-  `city` VARCHAR(50) NULL DEFAULT NULL,
+  `city` VARCHAR(30) NULL DEFAULT NULL,
+  `new field` INTEGER NULL DEFAULT NULL,
+  `zipcode` INTEGER(5) NULL DEFAULT NULL,
   `phone` VARCHAR(15) NULL DEFAULT NULL,
-  `email` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`, `org_id`)
 );
 
 -- ---
 -- Foreign Keys 
 -- ---
 
+ALTER TABLE `adopters` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
 ALTER TABLE `dogs` ADD FOREIGN KEY (breed) REFERENCES `breed` (`id`);
 ALTER TABLE `dogs` ADD FOREIGN KEY (org_id) REFERENCES `orgs` (`id`);
-ALTER TABLE `favoritedogs` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `favoritedogs` ADD FOREIGN KEY (user_id) REFERENCES `adopters` (`id`);
 ALTER TABLE `favoritedogs` ADD FOREIGN KEY (dog_id) REFERENCES `dogs` (`id`);
-ALTER TABLE `contacts(maybe)` ADD FOREIGN KEY (org_id) REFERENCES `orgs` (`id`);
+ALTER TABLE `users` ADD FOREIGN KEY (org_id) REFERENCES `orgs` (`id`);
 
 -- ---
 -- Table Properties
 -- ---
 
--- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `adopters` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `dogs` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `orgs` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `breed` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `favoritedogs` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `contacts(maybe)` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `users` (`id`,`name`,`pets`,`zipcode`,`house`) VALUES
--- ('','','','','');
+-- INSERT INTO `adopters` (`id`,`user_id`,`name`,`pets`,`zipcode`,`house`) VALUES
+-- ('','','','','','');
 -- INSERT INTO `dogs` (`id`,`name`,`breed`,`gender`,`size`,`temperament`,`age`,`fixed`,`medical`,`energy`,`photo`,`org_id`,`description`) VALUES
 -- ('','','','','','','','','','','','','');
--- INSERT INTO `orgs` (`id`,`name`,`location`,`contact`) VALUES
--- ('','','','');
+-- INSERT INTO `orgs` (`id`,`name`) VALUES
+-- ('','');
 -- INSERT INTO `breed` (`id`,`name`) VALUES
 -- ('','');
 -- INSERT INTO `favoritedogs` (`id`,`user_id`,`dog_id`) VALUES
 -- ('','','');
--- INSERT INTO `contacts(maybe)` (`id`,`name`,`org_id`,`address`,`city`,`phone`,`email`) VALUES
--- ('','','','','','','');
+-- INSERT INTO `users` (`id`,`username`,`password`,`email`,`org_id`,`address`,`city`,`new field`,`zipcode`,`phone`) VALUES
+-- ('','','','','','','','','','');
