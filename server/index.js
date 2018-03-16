6c984dd212ed7619a2eabbeed0e1b3e9f042e920
acdibble <acdibble@gmail.com>
2018-03-16 12:20:30 -0500

    Import and configure boilerplate

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser'); // eslint-disable-line
+
+const items = require('../database-mysql');
+
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../react-client/dist`));
+
+
+app.get('/items', (req, res) => {
+  items.selectAll((err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
+
