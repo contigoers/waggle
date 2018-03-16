d8e10916d4ca37c78562f30272918d468046f310
Shannon <schristinep@gmail.com>
2018-03-16 14:14:37 -0500

    removes boilerplate server request bc it's messing with deployment

--- server/index.js
+++ server/index.js
@@ -9,15 +9,15 @@ const app = express();
 app.use(express.static(`${__dirname}/../react-client/dist`));
 
 
-app.get('/items', (req, res) => {
-  items.selectAll((err, data) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+// app.get('/items', (req, res) => {
+//   items.selectAll((err, data) => {
+//     if (err) {
+//       res.sendStatus(500);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port 3000!');
