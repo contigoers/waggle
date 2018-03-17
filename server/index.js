9005c35185ca85bde46655fe26422ce878b571c0
acdibble <acdibble@gmail.com>
2018-03-16 19:53:57 -0500

    Add test routes to server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const Koa = require('koa');
+const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
 
 const app = new Koa();
-app.use(bodyParser());
 
-app.use(serve(`${__dirname}/../react-client/dist`));
+app
+  .use(serve(`${__dirname}/../react-client/dist`))
+  .use(bodyParser())
+  .use(router.routes());
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`listening on port ${3000}!`);
+router.get('/test', (ctx, next) => {
+  ctx.body = { message: 'hello' };
+  next();
+});
+
+router.post('/test', (ctx, next) => {
+  console.log(ctx.request.body);
+  ctx.status = 201;
+  next();
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}!`);
 });
