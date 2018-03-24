108ec5ab25b27e8ce9414129ee673a63413c512b
JP Angelle <jpangelle15@gmail.com>
2018-03-23 19:32:38 -0500

    removes polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const SRC_DIR = path.join(__dirname, '/react-client/src');
 const DIST_DIR = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
-  entry: ['babel-polyfill', `${SRC_DIR}/index.jsx`],
+  entry: [`${SRC_DIR}/index.jsx`],
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
