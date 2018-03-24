2d93d90389e68a3e3ddcf345eac4b774dda78531
JP Angelle <jpangelle15@gmail.com>
2018-03-24 10:53:57 -0500

    deletes the thing

--- react-client/src/actions/actionCreators.js
+++ react-client/src/actions/actionCreators.js
@@ -1,31 +1,22 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const TOGGLE_BUTTON = 'TOGGLE_BUTTON';
-// const UPDATE_RANDOM_PIC = 'UPDATE_RANDOM_PIC';
+const TOGGLE_BUTTON = 'TOGGLE_BUTTON';
+const UPDATE_RANDOM_PIC = 'UPDATE_RANDOM_PIC';
 
 
-// const sendMessage = () => (
-//   {
-//     type: TOGGLE_BUTTON,
-//   }
-// );
+const sendMessage = () => (
+  {
+    type: TOGGLE_BUTTON,
+  }
+);
 
-// const updateRandomPic = async () => {
-//   const { data } = await axios.get('/picture');
+const updateRandomPic = async () => {
+  const { data } = await axios.get('/picture');
 
-//   return {
-//     type: UPDATE_RANDOM_PIC,
-//     payload: data,
-//   };
-// };
+  return {
+    type: UPDATE_RANDOM_PIC,
+    payload: data,
+  };
+};
 
-// // const addDog = async () => {
-// //   const { data } = await axios.post('/createOrgDog');
-
-// //   return {
-// //     type: ADD_DOG, // ?
-// //     payload: data,
-// //   };
-// // };
-
-// export { TOGGLE_BUTTON, sendMessage, UPDATE_RANDOM_PIC, updateRandomPic };
+export { TOGGLE_BUTTON, sendMessage, UPDATE_RANDOM_PIC, updateRandomPic };
