13629d0438dedba0773fdec2a46f90c141626784
Shannon <schristinep@gmail.com>
2018-03-24 16:34:52 -0500

    MVP of getting a search results on search and dog profile on profile click (from url)

--- react-client/src/components/DogProfile.jsx
+++ react-client/src/components/DogProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Divider, Row, Col, Icon, message, Spin } from 'antd';
+import { Card, Divider, Row, Col, Icon, message } from 'antd';
 import { connect } from 'react-redux';
 import OrgCard from './OrgCard';
 
@@ -9,10 +9,9 @@ class DogProfile extends React.Component {
     super(props);
     this.state = {
       favorite: false,
-      dog: null,
-      org: null,
     };
     this.toggleFavorite = this.toggleFavorite.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   componentDidMount() {
@@ -35,92 +34,94 @@ class DogProfile extends React.Component {
 
   render() {
     console.log('props', this.props);
-    if (this.props.results) {
-      const dog = this.props.results.dogs['39'];
-      console.log(dog);
-      const org = this.props.results.orgs[dog.org_id];
-      let stage = dog.lifestage.charAt(0).toUpperCase() + dog.lifestage.slice(1);
-      if (dog.age) {
-        stage += ` (age ${dog.age})`;
-      }
+    const url = this.props.location.pathname;
+    const id = url.slice(5);
+    console.log(id);
 
-      let temperament = '';
-      if (dog.anxious && dog.aggressive) {
-        temperament = 'anxiety, aggression';
-      } else if (dog.anxious) {
-        temperament = 'anxiety';
-      } else if (dog.aggressive) {
-        temperament = 'aggression';
-      } else {
-        temperament = 'none';
-      }
+    const dog = this.props.results.dogs[id];
+    console.log(dog);
+    const org = this.props.results.orgs[dog.org_id];
 
-      let specialNeeds = '';
-      if (dog.diet && dog.medical) {
-        specialNeeds = 'diet, medical';
-      } else if (dog.diet) {
-        specialNeeds = 'diet';
-      } else if (dog.medical) {
-        specialNeeds = 'medical';
-      } else {
-        specialNeeds = 'none';
-      }
-
-      return (
-        <div>
-          <Row style={{ marginTop: 30, marginBottom: 30 }} >
-            <Col span={10} offset={3} >
-              <Card>
-                <h1> {dog.name} </h1>
-                <span style={{ fontWeight: 600, fontSize: 18, marginLeft: 5 }} > {dog.breed} {dog.mix ? 'mix' : ''} </span>
-                <Divider type="vertical" />
-                <span style={{ fontWeight: 600, fontSize: 16 }} > {dog.male ? 'Male' : 'Female'} </span>
-                <Divider type="vertical" />
-                <span style={{ fontWeight: 600, fontSize: 16 }} > {stage} </span>
-
-                <Divider />
-
-                <h2> About </h2>
-
-                <h3 style={{ marginLeft: 20 }}> Health </h3>
-                <div style={{ marginLeft: 40 }}>
-                  <span style={{ fontWeight: 700 }}> Size: </span> {dog.size}
-                </div>
-                <div style={{ marginLeft: 40 }}> <span style={{ fontWeight: 700 }}> Neutered/spayed: </span> {dog.fixed ? 'yes' : 'no'} </div>
-                <div style={{ marginLeft: 40 }}>
-                  <span style={{ fontWeight: 700 }}> Special needs: </span> {specialNeeds}
-                </div>
-
-                <h3 style={{ marginLeft: 20, marginTop: 20 }}> Behavior </h3>
-                <div style={{ marginLeft: 40 }}>
-                  <span style={{ fontWeight: 700 }}> Energy level: </span> {dog.energy_level}
-                </div>
-                <div style={{ marginLeft: 40 }}>
-                  <span style={{ fontWeight: 700 }}> Temperament concerns: </span> {temperament}
-                </div>
-
-                <h2 style={{ marginTop: 20 }} > Bio </h2>
-                <div style={{ marginLeft: 20 }} > {dog.description} </div>
-              </Card>
-            </Col>
-            <Col span={8} offset={1}>
-              <Card
-                style={{ width: 350 }}
-                cover={<img
-                  alt="pupper"
-                  src={dog.photo}
-                />}
-                actions={[<Icon onClick={this.toggleFavorite} type={this.state.favorite ? 'heart' : 'heart-o'} />]}
-              />
-            </Col>
-          </Row>
-          <Row style={{ marginBottom: 50 }} >
-            <OrgCard org={org} />
-          </Row>
-        </div>
-      );
+    let stage = dog.lifestage.charAt(0).toUpperCase() + dog.lifestage.slice(1);
+    if (dog.age) {
+      stage += ` (age ${dog.age})`;
     }
-    return (<Spin size="large" />);
+
+    let temperament = '';
+    if (dog.anxious && dog.aggressive) {
+      temperament = 'anxiety, aggression';
+    } else if (dog.anxious) {
+      temperament = 'anxiety';
+    } else if (dog.aggressive) {
+      temperament = 'aggression';
+    } else {
+      temperament = 'none';
+    }
+
+    let specialNeeds = '';
+    if (dog.diet && dog.medical) {
+      specialNeeds = 'diet, medical';
+    } else if (dog.diet) {
+      specialNeeds = 'diet';
+    } else if (dog.medical) {
+      specialNeeds = 'medical';
+    } else {
+      specialNeeds = 'none';
+    }
+
+    return (
+      <div>
+        <Row style={{ marginTop: 30, marginBottom: 30 }} >
+          <Col span={10} offset={3} >
+            <Card>
+              <h1> {dog.name} </h1>
+              <span style={{ fontWeight: 600, fontSize: 18, marginLeft: 5 }} > {dog.breed} {dog.mix ? 'mix' : ''} </span>
+              <Divider type="vertical" />
+              <span style={{ fontWeight: 600, fontSize: 16 }} > {dog.male ? 'Male' : 'Female'} </span>
+              <Divider type="vertical" />
+              <span style={{ fontWeight: 600, fontSize: 16 }} > {stage} </span>
+
+              <Divider />
+
+              <h2> About </h2>
+
+              <h3 style={{ marginLeft: 20 }}> Health </h3>
+              <div style={{ marginLeft: 40 }}>
+                <span style={{ fontWeight: 700 }}> Size: </span> {dog.size}
+              </div>
+              <div style={{ marginLeft: 40 }}> <span style={{ fontWeight: 700 }}> Neutered/spayed: </span> {dog.fixed ? 'yes' : 'no'} </div>
+              <div style={{ marginLeft: 40 }}>
+                <span style={{ fontWeight: 700 }}> Special needs: </span> {specialNeeds}
+              </div>
+
+              <h3 style={{ marginLeft: 20, marginTop: 20 }}> Behavior </h3>
+              <div style={{ marginLeft: 40 }}>
+                <span style={{ fontWeight: 700 }}> Energy level: </span> {dog.energy_level}
+              </div>
+              <div style={{ marginLeft: 40 }}>
+                <span style={{ fontWeight: 700 }}> Temperament concerns: </span> {temperament}
+              </div>
+
+              <h2 style={{ marginTop: 20 }} > Bio </h2>
+              <div style={{ marginLeft: 20 }} > {dog.description} </div>
+            </Card>
+          </Col>
+          <Col span={8} offset={1}>
+            <Card
+              style={{ width: 350 }}
+              cover={<img
+                alt="pupper"
+                src={dog.photo}
+              />}
+              actions={[<Icon onClick={this.toggleFavorite} type={this.state.favorite ? 'heart' : 'heart-o'} />]}
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: 50 }} >
+          <OrgCard org={org} />
+        </Row>
+      </div>
+    );
   }
 }
 
