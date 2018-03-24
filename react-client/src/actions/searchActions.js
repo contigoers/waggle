2765897a4ff365995e8ebaa5be058703cedc76d6
JP Angelle <jpangelle15@gmail.com>
2018-03-23 19:44:47 -0500

    Revert "removes async await from an action"
    This reverts commit 92872454477b55cac8db1023a8a46be1150a467c.

--- react-client/src/actions/searchActions.js
+++ react-client/src/actions/searchActions.js
@@ -1,24 +1,24 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const UPDATE_SEARCH_QUERY = 'UPDATE_SEARCH_QUERY';
-// const SEARCH_DOGS = 'SEARCH_DOGS';
+const UPDATE_SEARCH_QUERY = 'UPDATE_SEARCH_QUERY';
+const SEARCH_DOGS = 'SEARCH_DOGS';
 
-// const updateSearchQuery = (id, value, checked) =>
-//   (
-//     {
-//       type: UPDATE_SEARCH_QUERY,
-//       id,
-//       value,
-//       checked,
-//     }
-//   );
+const updateSearchQuery = (id, value, checked) =>
+  (
+    {
+      type: UPDATE_SEARCH_QUERY,
+      id,
+      value,
+      checked,
+    }
+  );
 
-// const dogsSearch = async (searchObject) => {
-//   const { data } = await axios.post('/searchOrgDogs', searchObject);
-//   return {
-//     type: SEARCH_DOGS,
-//     data: data.dogsAndOrgs,
-//   };
-// };
+const dogsSearch = async (searchObject) => {
+  const { data } = await axios.post('/searchOrgDogs', searchObject);
+  return {
+    type: SEARCH_DOGS,
+    data: data.dogsAndOrgs,
+  };
+};
 
-// export { UPDATE_SEARCH_QUERY, updateSearchQuery, SEARCH_DOGS, dogsSearch };
+export { UPDATE_SEARCH_QUERY, updateSearchQuery, SEARCH_DOGS, dogsSearch };
