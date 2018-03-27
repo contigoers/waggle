289c9aef5640281033602e2c1f538fe19c2ef785
acdibble <acdibble@gmail.com>
2018-03-27 16:37:08 -0500

    Clean up reducers

--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -1,18 +1,12 @@
 import { combineReducers } from 'redux';
-import showMessage from './reducer_show-message';
-import randomDogPic from './random-dog-pic';
 import registrationModal from './registrationReducers';
 import search from './searchReducers';
-import dummyProfile from './dummyProfile';
 import { loginModal, storeUser } from './loginReducers';
 import searchSelections from './searchViewReducers';
 
 const rootReducer = combineReducers({
-  contents: showMessage,
-  randomDogPic,
   registrationModal,
   search,
-  profile: dummyProfile,
   loginModal,
   storeUser,
   searchSelections,
