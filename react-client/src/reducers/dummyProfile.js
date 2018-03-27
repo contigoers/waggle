6f114c14ec8aecf80d7dbe0f17a119a42d4370cc
acdibble <acdibble@gmail.com>
2018-03-27 17:35:22 -0500

    Add dummy profile back in

---
+++ react-client/src/reducers/dummyProfile.js
@@ -0,0 +1,37 @@
+const defaultState = {
+  dog: {
+    name: 'Tractor',
+    breed: 'Chihuahua',
+    mix: true,
+    male: true,
+    size: 'small',
+    anxious: true,
+    aggressive: false,
+    lifestage: 'adult',
+    age: 7,
+    fixed: true,
+    diet: true,
+    medical: false,
+    energy_level: 'medium',
+    photo: 'https://static01.nyt.com/images/2018/02/11/realestate/11dogs-topbreeds-Chihuahua/11dogs-topbreeds-Chihuahua-master495.jpg',
+    description: 'a Good Boy',
+  },
+  org: {
+    name: 'Austin Pets Alive!',
+    phone: '5129616519',
+    address: '1156 W Cesar Chavez St',
+    city: 'Austin',
+    state: 'TX',
+    zipcode: 78703,
+    email: 'adopt@austinpetsalive.org',
+    username: 'orguser123',
+    id: 2,
+  },
+  adopter: {
+    id: 1,
+  },
+};
+
+const dummyProfile = (state = defaultState) => state;
+
+export default dummyProfile;
--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import registrationModal from './registrationReducers';
 import search from './searchReducers';
+import dummyProfile from './dummyProfile';
 import { loginModal, storeUser } from './loginReducers';
 import searchSelections from './searchViewReducers';
 
 const rootReducer = combineReducers({
   registrationModal,
   search,
+  profile: dummyProfile,
   loginModal,
   storeUser,
   searchSelections,
