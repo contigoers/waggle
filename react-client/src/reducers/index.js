313020fc9b5a78c5e0581066e9d084d09e85751b
acdibble <acdibble@gmail.com>
2018-03-28 21:24:14 -0500

    Remove dummy profile

--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import registrationModal from './registrationReducers';
 import search from './searchReducers';
-import dummyProfile from './dummyProfile';
 import { loginModal, storeUser } from './loginReducers';
 import searchSelections from './searchViewReducers';
 import inquiryModal from './messagingReducers';
@@ -9,7 +8,6 @@ import inquiryModal from './messagingReducers';
 const rootReducer = combineReducers({
   registrationModal,
   search,
-  profile: dummyProfile,
   loginModal,
   storeUser,
   searchSelections,
