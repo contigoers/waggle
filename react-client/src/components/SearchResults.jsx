2da6a7b77409adecbe1618f382cf6cd1e83453bb
Shannon <schristinep@gmail.com>
2018-04-05 00:32:01 -0500

    paginate search results

--- react-client/src/components/SearchResults.jsx
+++ react-client/src/components/SearchResults.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { map, isEmpty } from 'lodash';
+import { Pagination } from 'antd';
 import DogCard from './DogPreviewCard';
 
-const SearchResults = props => (
-  <div className="search-results-grid" style={{ marginTop: 30 }} >
-    {!isEmpty(props.dogs) ? map(props.dogs, dog => (<DogCard key={dog.id} dog={dog} />)) : 'No Results'}
-  </div>
-);
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleDogIds: [],
+    };
+    this.onPageChange = this.onPageChange.bind(this);
+    this.ids = Object.keys(this.props.dogs);
+  }
+
+  componentWillMount() {
+    console.log(this.ids)
+    this.setState({ visibleDogIds: this.ids.slice(0, 12) });
+  }
+
+  onPageChange(page, pageSize) {
+    this.setState({
+      visibleDogIds: this.ids.slice((page * pageSize) - 1, ((page * pageSize) - 1) + pageSize),
+    });
+  }
+
+  render() {
+    console.log('IDS: ', Object.keys(this.props.dogs));
+    const { dogs } = this.props;
+    console.log('DOGS', dogs);
+
+    return (
+      <div>
+        <div className="search-results-grid" style={{ marginTop: 30 }} >
+          {!isEmpty(dogs) ? map(this.state.visibleDogIds, dogId => (<DogCard key={dogs[dogId].id} dog={dogs[dogId]} />)) : 'No Results'}
+        </div>
+        <Pagination
+          // current page = 1 on render
+          defaultPageSize={12}
+          total={this.ids.length}
+          onChange={this.onPageChange}
+        />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => (
   {
